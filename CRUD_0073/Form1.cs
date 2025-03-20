3b12617bb79bc0e67268ce3b9aa80344de221d78
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-03-20 18:14:20 +0700

    memastikan data valid

--- CRUD_0073/Form1.cs
+++ CRUD_0073/Form1.cs
@@ -128,4 +128,9 @@ namespace CRUD_0073
             }
         }
 
+        private void dgvMahasiswa_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+
 
