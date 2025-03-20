0728a1f81563db93ea44d015e2e72a179ac3b6e4
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-03-20 17:56:50 +0700

    untuk memuat data base ke sql server

--- CRUD_0073/Form1.cs
+++ CRUD_0073/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -34,3 +35,8 @@ namespace CRUD_0073
             txtAlamat.Clear();
             txtNIM.Focus();
         }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
