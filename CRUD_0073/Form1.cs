3c74cf6a97fda14ae060e438104b1eb097475254
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-03-20 18:10:54 +0700

    menghubungkan ke database

--- CRUD_0073/Form1.cs
+++ CRUD_0073/Form1.cs
@@ -98,4 +98,7 @@ namespace CRUD_0073
             {
                 DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (confirm == DialogResult.Yes)
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
 
