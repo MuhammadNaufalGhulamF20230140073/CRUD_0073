30475f9ea4200541230cf510264a7693cb1c7d17
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-03-20 18:05:05 +0700

    menambahkan nim yang sudah disi

--- CRUD_0073/Form1.cs
+++ CRUD_0073/Form1.cs
@@ -73,3 +73,5 @@ namespace CRUD_0073
                     conn.Open();
                     string query = "INSERT INTO Mahasiswa (NIM, Nama, Email, Telepon, Alamat) VALUES (@NIM, @Nama, @Email, @Telepon, @Alamat)";
                     using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@NIM", txtNIM.Text.Trim());
